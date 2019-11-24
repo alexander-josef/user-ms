83fe61f79606027871c0f4a5769051c3dc285bc4
Alexander Josef <alexander.josef@netcetera.com>
2019-11-24 16:53:17 +0100

    adapting custom mongodb connection

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import * as winston from 'winston'
 import { APIRoute } from './routes/api'
 import { UserRoute } from './routes/user'
 import * as errorHandler from './utility/errorHandler'
+import { OrderAPILogger } from './utility/logger'
 
 class App {
   public app: express.Application
@@ -15,14 +16,19 @@ class App {
   public mongoUrl: string
   public mongoUser: string
   public mongoPass: string
+  // public mongoUrl: string = 'mongodb://localhost/order-api' -> use env variables instead
+
 
   constructor() {
     const path = `${__dirname}/../.env.${process.env.NODE_ENV}`
 
-    dotenv.config({ path: path })
-    this.mongoUrl = `mongodb://${process.env.MONGODB_URL_PORT}/${
+    dotenv.config({ path: path }) // -> this creates env variables according to the . file of the active environment the process is running in?
+/*     this.mongoUrl = `mongodb://${process.env.MONGODB_URL_PORT}/${
       process.env.MONGODB_DATABASE
     }`
+*/
+
+    this.mongoUrl = `${process.env.MONGODB_URL_PORT}/${process.env.MONGODB_DATABASE}` // connection string for dev/test
     this.mongoUser = `${process.env.MONGODB_USER}`
     this.mongoPass = `${process.env.MONGODB_PASS}`
 
@@ -48,13 +54,16 @@ class App {
       options = {
         useNewUrlParser: true,
       }
-    } else {
+    } else { // different handling for prod (hosted atlas mongoDB with username / password)
+      this.mongoUrl = `${process.env.MONGODB_URL_PORT}/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority` // different URI for Atlas connection in prod
       options = {
         user: this.mongoUser,
         pass: this.mongoPass,
         useNewUrlParser: true,
       }
     }
+    OrderAPILogger.logger.info(`using connection string: ${this.mongoUrl} - user: ${this.mongoUser} - pass: ${this.mongoPass}`)
+
     mongoose.connect(
       this.mongoUrl,
       options
